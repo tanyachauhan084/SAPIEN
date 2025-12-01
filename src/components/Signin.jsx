d6368c2c83e06d38118c6c80f92277c77b3fb75a
Tanya Chauhan <tanyachauhan084@gmail.com>
2025-12-01 15:52:01 +0530

    created Signin.jsx file

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Signin = () => {
   return (
-    <div>Signin</div>
+    <div className=' h-screen text-center flex justify-center items-center'>
+
+        <div className='h-80 w-80 bg-ocean-blue rounded-lg'>
+
+<h1 className='pt-4 text-center text-2xl text-white font-bold'>SAPIEN</h1>
+<div>
+<h1>Sign in</h1>
+</div>
+        </div>
+
+    </div>
   )
 }
 
