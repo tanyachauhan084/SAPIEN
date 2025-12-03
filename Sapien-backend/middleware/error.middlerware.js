83729a4462f70845108425ea9d0589f306ffda03
Tanya Chauhan <tanyachauhan084@gmail.com>
2025-12-03 14:36:23 +0530

    Added - errorMiddleware

---
+++ Sapien-backend/middleware/error.middlerware.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const errorMiddleware = (err, req, res,next) => {
+  const status= err.statusCode || 500;
+  const message= err.message|| "something wnet wrong";
+
+  res.status(status).json({
+    success:false,
+    message: message,
+  })
+}
+
+export default errorMiddleware
